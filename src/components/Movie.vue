9e34f6e9d2e85947a364dcfb2203ca7c7beab850
sofien <81614240+sofienBNA@users.noreply.github.com>
2021-10-15 15:16:28 +0200

    first

---
+++ src/components/Movie.vue
@@ -0,0 +1,82 @@
+<template>
+
+  <v-app>
+    <div class="list">
+      <h3>{{ movie.id }}/ {{ movie.title.toUpperCase() }}</h3>
+      <a :to="{name:'Movie', params:{id: movie.id}}">
+        <router-link :to="{name:'Movie', params:{id:movie.id, movie:movie}}">
+          <img class="imgMovie" :src="movie.image" width="300px"/>
+        </router-link>
+      </a>
+      <p> Genre:</p>
+      <ol class="genre" v-for="item in movie.genres" :key="item"> {{ item.toUpperCase() }}</ol>
+      <v-rating
+          class="rate"
+          :value="movie.rating"
+          background-color="red lighten-3"
+          color="yellow"
+          length="10"
+          large
+          readonly
+      ></v-rating>
+    </div>
+  </v-app>
+
+
+</template>
+
+<script>
+export default {
+  name: "Movie",
+  props: {
+    id: {
+      type: Number,
+      default: null
+    },
+    title: {
+      type: String,
+      default: "aucun"
+    },
+    image: {
+      type: String,
+      default: "https://img.daf-mag.fr/Img/BREVE/2018/10/334316/Tribune-URSSAF-droit-erreur-vos-nouveaux-rapports-administration-F.jpg",
+    },
+    genres: {
+      type: Array,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    review: {
+      type: String,
+      default: "aucun avis",
+    },
+    description: {
+      type: String,
+      default: "aucune description"
+    },
+    movie: Object,
+  },
+  data() {
+    return {}
+  },
+
+}
+</script>
+
+<style scoped>
+.imgMovie {
+  border: groove
+}
+
+.list{
+  color: white
+;
+}
+.genre{
+  display:inline-flex;
+}
+
+
+</style>
---
+++ src/components/MovieCreation.vue
@@ -0,0 +1,215 @@
+<template>
+  <v-app>
+    <v-form
+        ref="form"
+        v-model="nouveau.valid"
+        lazy-validation
+    >
+      <h2 class="form_title"> Ajout de Film</h2>
+      <v-container>
+        <v-row>
+          <v-spacer/>
+          <v-col cols="12" md="4">
+            <v-text-field
+                class="input"
+                v-model="nouveau.title"
+                :rules="nouveau.titleRules"
+                label="TITRE"
+                required
+                placeholder="Champ obligatoire"
+                dense
+                rounded
+                color="success"
+            ></v-text-field>
+          </v-col>
+          <v-spacer/>
+        </v-row>
+        <v-row>
+          <v-spacer/>
+          <v-col cols="12" md="4">
+            <v-text-field
+                class="input"
+                v-model="nouveau.image"
+                :rules="nouveau.imageRules"
+                label="URL IMAGE"
+                placeholder="Rentrez un URL"
+                dense
+                rounded
+                color="success"
+                required
+            ></v-text-field>
+          </v-col>
+          <v-spacer/>
+        </v-row>
+        <v-row>
+          <v-spacer/>
+          <v-rating class="rating "
+                    v-model="nouveau.rating"
+                    background-color="red "
+                    color="yellow"
+                    large
+                    hover
+                    length="10"
+                    size="10"
+                    required
+          ></v-rating>
+          <v-spacer/>
+        </v-row>
+
+        <!--checkbox-->
+
+        <v-card-text>
+          <v-container fluid>
+            <v-row>
+              <v-col
+                  cols="12"
+                  sm="4"
+                  md="4"
+              >
+                <v-checkbox
+                    v-model="nouveau.genres"
+                    label="action"
+                    color="red"
+                    value="action"
+                    dark
+                ></v-checkbox>
+                <v-checkbox
+                    v-model="nouveau.genres"
+                    label="Science-Fiction"
+                    color="purple"
+                    value="Science-Fiction"
+                    dark
+                ></v-checkbox>
+              </v-col>
+              <v-col
+                  cols="12"
+                  sm="4"
+                  md="4"
+              >
+                <v-checkbox
+                    v-model="nouveau.genres"
+                    label="Fantastique"
+                    color="indigo"
+                    value="Fantastique"
+                    dark
+                ></v-checkbox>
+                <v-checkbox
+                    v-model="nouveau.genres"
+                    label="Horreur"
+                    color="orange"
+                    value="Horreur"
+                    dark
+                ></v-checkbox>
+              </v-col>
+              <v-col
+                  cols="12"
+                  sm="4"
+                  md="4"
+              >
+                <v-checkbox
+                    v-model="nouveau.genres"
+                    label="Comedie"
+                    color="green"
+                    value="Comedie"
+                    dark
+                ></v-checkbox>
+                <v-checkbox
+                    v-model="nouveau.genres"
+                    label="Documentaire"
+                    color="white"
+                    value="Documentaire"
+                    dark
+                ></v-checkbox>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+
+        <!--          boutton-->
+<!--        <v-row>-->
+<!--          <v-spacer/>-->
+<!--          <div class="text-center">-->
+<!--            <v-btn-->
+<!--                color="success"-->
+<!--                class="mr-4"-->
+<!--                @click="addMovieEvent"-->
+<!--            >-->
+        <v-row>
+          <v-spacer/>
+          <div class="text-center">
+            <router-link :to="{name:'Home'}" >
+            <v-btn
+                color="success"
+                class="mr-4"
+                @click="emitGlobalClickEvent()"
+            >
+              Ajoutez Film
+            </v-btn>
+            </router-link>
+          </div>
+          <v-spacer/>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-app>
+</template>
+
+<script>
+import {EventBus} from '/src/event-bus';
+export default {
+  name: "MovieCreation",
+  props: {
+    movies: Array,
+    addMovie: Function
+  },
+  data() {
+    return {
+      nouveau: {
+        valid: true,
+        id: null,
+        title: null,
+        titleRules: [v => !!v || 'Name is required'],
+        image: null,
+        imageRules: [v => !!v || 'URL is required'],
+        genres: [],
+        rating: null,
+      },
+    };
+  },
+  // addMovie() {
+  //   this.movies.push({
+  //         id: this.movies.length + 1,
+  //         title: this.nouveau.title,
+  //         image: this.nouveau.image,
+  //         genres: this.nouveau.genres,
+  //         rating: this.nouveau.rating,
+  //         review: this.nouveau.review,
+  //         description: this.nouveau.description
+  //       }
+  //   )
+  //   console.log(this.nouveau)
+  //   // .then(alert("Création réussie"));
+  // },
+  // reset() {
+  //   this.$refs.form.reset()
+  // },
+  methods: {
+
+    emitGlobalClickEvent() {
+      EventBus.$emit('clicked', this.nouveau);
+    }
+    // addMovieEvent() {
+    //   this.$emit('addMovie-event', this.nouveau )
+    // },
+    // resetEvent(){
+    //   this.$emit('reset-event',this.plaload.$refs.form.reset())
+    // }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
